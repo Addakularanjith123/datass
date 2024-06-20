9b2959208d6bd2203fed501c73613c64d51e0a53
addak <test@test.com>
2024-06-21 03:07:08 +0530

    function

--- using functions.py
+++ using functions.py
@@ -1,10 +1,16 @@
-def factorial(num):
-    prod = 1
-    for i in range(1, num+ 1):
-        prod = prod * i
-    factorial=prod
-    print(factorial)
-    print(prod)
-    n_rfact = prod
-    return factorial
-print(f"the fuction{factorial(4)}")
+# def factorial(num):
+#     prod = 1
+#     for i in range(1, num+ 1):
+#         prod = prod * i
+#     factorial=prod
+#     print(factorial)
+#     print(prod)
+#     n_rfact = prod
+#     return factorial
+# print(f"the fuction{factorial(4)}")
+#
+#
+
+#LAMDA
+ def add_n(x):
+     return x+10
