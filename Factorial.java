632e3852b29a28cc4d04300f1cd26f33acbeff1b
addak <test@test.com>
2024-07-18 10:40:00 +0530

    jul18 work

---
+++ Factorial.java
@@ -0,0 +1,23 @@
+public class Factorial
+{
+	public static void main(String[] args)
+	{
+	System.out.println("Hai Ranjith");
+	System.out.println("fuck off");
+	int a=10,b=20;
+	System.out.println("Before Swapping.." +a+ "  " +b);
+	/*int t=a;
+	a=b;
+	b=t;
+	a=a+b;
+	b=a-b;
+	a=a-b;*/
+	a=a*b;
+	b=a/b;
+	a=a/b;
+	System.out.println("After Swapping.." +a+ "  " +b);
+	System.out.println("After Swapping.." +a);
+	
+
+	}
+}
---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+class Multiplication
+{
+	public static void main(String[]args)
+	{
+		int a=90,b=90,c=a*b;
+		System.out.println("The multiplication of no="+c);
+		System.out.print("fuck off");
+	}
+}
